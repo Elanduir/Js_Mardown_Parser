40582e10534e3ca2751495f752dde4d486129ee9
Fabian Heuberger <elanduir@gmail.com>
2023-06-01 14:13:59 +0200

    Fixed order of splicing lists in

--- src/parser.js
+++ src/parser.js
@@ -20,23 +20,24 @@ const Parse = (text) => {
 
   text = text.replaceAll(brReg, "\n<br>\n");
   text = text.replaceAll(hrReg, "<hr>");
-  console.log(text);
   text = parseCode(text);
   text = parseImages(text);
   text = parseHyperlinks(text);
   text = parseTables(text);
 
   let allLines = text.split(/\n/);
-  let extract = extractAllLists(allLines);
-  allLines = extract[0].join(" ").split("<br>");
-  let listHtml = parseLists(extract[1]);
 
+  let extract = extractAllLists(allLines);
+  allLines = extract[0];
+  let listHtml = parseLists(extract[1]);
+  listHtml.forEach((html) => {
+    allLines.splice(html[0], 0, html[1]);
+  });
+  allLines = allLines.join(" ").split("<br>");
   allLines.forEach((line) => {
     let pL = parseLine(line);
     parsedLines.push(pL);
   });
-
-  listHtml.forEach((html) => parsedLines.splice(html[0], 0, html[1]));
   return parsedLines.join("\n");
 };
 
@@ -47,7 +48,7 @@ const parseLine = (line) => {
   let mod = splitLine[0];
   switch (true) {
     case ulReg.test(line) || olReg.test(line):
-      break;
+      return line;
     case mod.trim().charAt(0) === "#":
       return header(splitLine.slice(1).join(" "), mod.length);
     case line === "<br>":
